3a674900c6c5c75ffbd7a0c8f75acd6d933bce6b
chrislee0419 <cla235@sfu.ca>
2016-03-17 17:01:17 -0700

    sensitivity changing works

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # PAYDAY 2 - Mouse Sensitivity Additions Mod
 
-At this stage, this mod plans to add two features (neither of which currently work): 
+### Currently Implemented
 - enable players to set a mouse sensitivity outside of the default range of 0.3 to 1.7 (expanded to 0.05 to 2)
-- allow players to set a secondary mouse sensitivity, which can be used on-demand via a keybind
+- allow players to set a secondary mouse sensitivity, which can be toggled via keybind
+
+### Planned Features
+- choice between toggle and hold to use secondary sensitivity
+- add option to use different sensitivity when aiming down the sights (as implemented in the default controls menu)
--- lua/mousesensitivity.lua
+++ lua/mousesensitivity.lua
@@ -4,6 +4,7 @@ _G.MouseSensitivityAdditions = _G.MouseSensitivityAdditions or {}
 MouseSensitivityAdditions._path = ModPath
 MouseSensitivityAdditions._data_path = SavePath .. "msa_options.txt"
 MouseSensitivityAdditions._data = {}
+MouseSensitivityAdditions.using_value = "primary"
 
 --[[
 	SAVING AND LOADING USER SETTINGS
@@ -31,9 +32,23 @@ end
 	HELPER FUNCTIONS
 ]]
 
-function MouseSensitivityAdditions:SetSensitivity(menu_manager, value)
-	managers.user:set_setting("camera_sensitivity", value)
-	menu_manager:camera_sensitivity_changed(nil, nil, value)
+function MouseSensitivityAdditions:SetUsage( value )
+	MouseSensitivityAdditions.using_value = value
+end
+
+function MouseSensitivityAdditions:GetUsage()
+	return MouseSensitivityAdditions.using_value
+end
+
+function MouseSensitivityAdditions:Toggle()
+	if MouseSensitivityAdditions.GetUsage == "primary" then
+		managers.user:set_setting( "camera_sensitivity", MouseSensitivityAdditions._data.secondary_sensitivity )
+		MouseSensitivityAdditions.SetUsage( "secondary" )
+	else
+		managers.user:set_setting( "camera_sensitivity", MouseSensitivityAdditions._data.primary_sensitivity )
+		MouseSensitivityAdditions.SetUsage( "primary" )
+	end
+	menu_manager:set_mouse_sensitivity(false)
 end
 
 --[[
@@ -51,23 +66,24 @@ Hooks:Add("MenuManagerInitialize", "MSAMenuManagerInitialize", function( menu_ma
 	-- Define menu callback functions
 	MenuCallbackHandler.MSASetPrimarySensitivity = function(self, item)
 		MouseSensitivityAdditions._data.primary_sensitivity = item:value()
-		-- Set sensitivity changes immediately
-		managers.user:set_setting( "camera_sensitivity", MouseSensitivityAdditions._data.primary_sensitivity )
-		menu_manager:set_mouse_sensitivity(false)
+		if MouseSensitivityAdditions.GetUsage() == "primary" then
+			managers.user:set_setting( "camera_sensitivity", MouseSensitivityAdditions._data.primary_sensitivity )
+			menu_manager:set_mouse_sensitivity(false)
+		end
 		MouseSensitivityAdditions:Save()
 	end
 
 	MenuCallbackHandler.MSASetSecondarySensitivity = function(self, item)
 		MouseSensitivityAdditions._data.secondary_sensitivity = item:value()
-		MouseSensitivityAdditions:Save()
-	end
-
-	MenuCallbackHandler.MSASetToggle = function(self, item)
-		MouseSensitivityAdditions._data.toggle_boolean = ( item:value() == "on" )
+		if MouseSensitivityAdditions.GetUsage() == "secondary" then
+			managers.user:set_setting( "camera_sensitivity", MouseSensitivityAdditions._data.secondary_sensitivity )
+			menu_manager:set_mouse_sensitivity(false)
+		end
 		MouseSensitivityAdditions:Save()
 	end
 
 	MouseSensitivityAdditions.MSAActivateSecondarySensitivity = function(self)
+		MouseSensitivityAdditions.Toggle()
 	end
 
 	-- Create custom menus
